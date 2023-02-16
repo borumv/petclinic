07f8a00e95820825ca360fb781428cb25d7d8ddd
Falltroy <fallTroy@protonmail.com>
2023-02-16 08:56:44 +0300

    adding Pojo. Closes #1

---
+++ petclinic/src/main/java/com/example/petclinic/models/Pet.java
@@ -0,0 +1,35 @@
+package com.example.petclinic.models;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType petType;
+
+    private Owner owner;
+    private LocalDate birthdate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+}
