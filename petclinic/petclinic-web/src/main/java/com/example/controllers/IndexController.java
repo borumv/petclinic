82564dd282b97290d9ebece81bed5206e5d5e9f5
Falltroy <fallTroy@protonmail.com>
2023-02-19 10:22:58 +0300

    Create Index page and Controller

---
+++ petclinic/petclinic-web/src/main/java/com/example/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/", "index","", "index.html"})
+    public String index(){
+        return "index";
+    }
+
+}
