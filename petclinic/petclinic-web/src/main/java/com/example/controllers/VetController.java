a60b0f93a6ce1c856b8f8a6e96d8c2037fbc1fff
Falltroy <fallTroy@protonmail.com>
2023-02-27 11:28:08 +0300

    List all vets on Vets index page

---
+++ petclinic/petclinic-web/src/main/java/com/example/controllers/VetController.java
@@ -0,0 +1,29 @@
+package com.example.controllers;
+
+
+import com.example.models.Vet;
+import com.example.services.OwnerService;
+import com.example.services.VetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Set;
+
+@RequestMapping("vets")
+@Controller
+public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/", "/index", "index.html", ""})
+    public String owners(Model model){
+        model.addAttribute("vets", vetService.findAll());
+        return "vets/index";
+    }
+}
