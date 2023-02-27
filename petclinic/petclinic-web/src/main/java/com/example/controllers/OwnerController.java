668fab3e10f45896dfd4883a5372d5f7c55a6d5c
Falltroy <fallTroy@protonmail.com>
2023-02-27 10:37:31 +0300

    List all owners on Owners index page

--- petclinic/petclinic-web/src/main/java/com/example/controllers/OwnerController.java
+++ petclinic/petclinic-web/src/main/java/com/example/controllers/OwnerController.java
@@ -1,14 +1,20 @@
 package com.example.controllers;
 
+import com.example.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("owners")
 @Controller
 public class OwnerController {
-
+    private final OwnerService ownerService;
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
     @RequestMapping({"/", "/index", "index.html", ""})
-    public String owners(){
+    public String owners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         System.out.println("tralala");
         return "owners/index";
     }
