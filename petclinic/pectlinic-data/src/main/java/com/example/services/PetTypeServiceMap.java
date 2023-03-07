409ec0f8f74dafb6dea6a9e32edac7c3415434ba
Falltroy <fallTroy@protonmail.com>
2023-03-07 10:42:20 +0300

    Create PetType Service closes #13

--- petclinic/pectlinic-data/src/main/java/com/example/services/PetTypeServiceMap.java
+++ petclinic/pectlinic-data/src/main/java/com/example/services/PetTypeServiceMap.java
@@ -1,7 +1,9 @@
 package com.example.services;
 
 import com.example.models.PetType;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PetTypeServiceMap extends AbstractCrudServiceMap<PetType, Long> implements PetTypeService{
     @Override
     public PetType save(PetType petType) {
