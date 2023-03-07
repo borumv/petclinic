01c23cd81f7705bbf9cda5b99cef6b27698e252b
Falltroy <fallTroy@protonmail.com>
2023-03-07 12:48:36 +0300

    Create Specialty Service closes #15

--- petclinic/pectlinic-data/src/main/java/com/example/services/SpecialityServiceMap.java
+++ petclinic/pectlinic-data/src/main/java/com/example/services/SpecialityServiceMap.java
@@ -1,7 +1,9 @@
 package com.example.services;
 
 import com.example.models.Speciality;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SpecialityServiceMap extends AbstractCrudServiceMap<Speciality, Long> implements SpecialitiesService{
 
 }
