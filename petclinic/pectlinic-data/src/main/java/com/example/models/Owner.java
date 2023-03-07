fb316a2c11ebfa59ec6324557ac139d06c1641e1
Falltroy <fallTroy@protonmail.com>
2023-03-07 10:09:46 +0300

    Create Pet Type, Pet and Visit Entities closes #10

--- petclinic/pectlinic-data/src/main/java/com/example/models/Owner.java
+++ petclinic/pectlinic-data/src/main/java/com/example/models/Owner.java
@@ -1,5 +1,15 @@
 package com.example.models;
 
-public class Owner extends Person{
+import java.util.Set;
 
+public class Owner extends Person{
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
--- petclinic/pectlinic-data/src/main/java/com/example/models/Pet.java
+++ petclinic/pectlinic-data/src/main/java/com/example/models/Pet.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 public class Pet  extends BaseEntity{
 
     private PetType petType;
-
     private Owner owner;
     private LocalDate birthdate;
 
