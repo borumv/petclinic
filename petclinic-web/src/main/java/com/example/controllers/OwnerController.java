13ff7c425fbd4473a340084cde88ca644b40cd14
Falltroy <fallTroy@protonmail.com>
2023-04-21 14:22:48 +0300

    update

--- petclinic-web/src/main/java/com/example/controllers/OwnerController.java
+++ petclinic-web/src/main/java/com/example/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.example.controllers;
 import com.example.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.yaml.snakeyaml.emitter.ScalarAnalysis;
 
 import java.util.Scanner;
@@ -24,5 +27,11 @@ public class OwnerController {
     @RequestMapping({"/find"})
     public String findOwners(Model model){
         return "notimplementedyet";
+    }
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner (@PathVariable("ownerId") Long id){
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject("owner", ownerService.findById(id));
+        return mav;
     }
 }
